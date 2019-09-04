c1d7e563b3a3a3b10cffeeb29749b825fc12d518
Lucy Johnston <ljohnston931@gmail.com>
2019-09-03 18:24:17 -0600

    Add advertise button

--- src/components/results.js
+++ src/components/results.js
@@ -3,6 +3,16 @@ import Result from './result.js';
 
 class Results extends Component {
 
+    openAdvertiseModal() {
+        const form =
+            <form className="modalForm" method="POST" action="https://formspree.io/ljohnston931@gmail.com">
+                <textarea name="message" placeholder="Message"></textarea>
+                <input type="email" name="email" placeholder="Your email"></input>
+                <button className="pill" type="submit">Email Me</button>
+            </form>;
+        this.props.openModal("CONTACT ME", form);
+    }
+
     render() {
         if (this.props.results) {
             // if (error) sorry I only get 100 searches a day before google starts charging me money. Try again tomorrow!
@@ -16,6 +26,7 @@ class Results extends Component {
         return (
             <div>
                 <p>{message}</p>
+                <div className="pill" onClick={() => this.openAdvertiseModal()}>Advertise your playlist here!</div>
                 <div>{results}</div>
             </div>
         );} else {
